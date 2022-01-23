e9ca897c98aea66b478cd088272a24a7060f80c2
Volodymyr Shynkarov <djevl333@gmail.com>
2022-01-23 03:25:51 +0200

    New: Standartize, Shift, Auto-delete Anomalies, Emper.distr. func

--- MathStatF.py
+++ MathStatF.py
@@ -1,28 +1,29 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import norm
 import PySimpleGUI as sg
 from stats import *
 
 
-def statistic_analysis(mas):
+def statistic_analysis(data):
     text = ''
-    text += 'Summ: ' + str(round(sum(mas), 4)) + '\nMax: ' + str(max(mas)) + '\nMin: ' + str(
-        min(mas)) + '\nMid: ' + str(mid(mas)) + '\nScale: ' + str(
-        scale(mas)) + '\nMe: ' + str(me(mas)) + '\nMed: ' + str(med(mas)) + '\nVar: ' + str(
-        var(mas)) + '\nStDev: ' + str(standart_d(mas)) + '\nAsym: '
-    return text + str(asymmetry(mas)) + '\nEcs: ' + str(
-        kurtosis(mas)) + '\nCEcs: ' + str(
-        c_kurtosis(mas)) + '\nPirson: ' + str(pirson(mas)) + '\nMedU: ' + str(med_oul(mas)) + '\nQuantiles: ' + str(
-        quant(mas))
+    text += 'Summ: ' + str(round(sum(data), 4)) + '\nMax: ' + str(round(max(data), 5)) + '\nMin: ' + str(
+        round(min(data), 5)) + '\nMid: ' + str(mid(data)) + '\nScale: ' + str(
+        scale(data)) + '\nMean: ' + str(mean(data)) + '\nMed: ' + str(med(data)) + '\nVar: ' + str(
+        var(data)) + '\nStDev: ' + str(standart_d(data)) + '\nAsym: '
+    return text + str(asymmetry(data)) + '\nEcs: ' + str(
+        kurtosis(data)) + '\nCEcs: ' + str(
+        c_kurtosis(data)) + '\nPirson: ' + str(pirson(data)) + '\nMedU: ' + str(med_oul(data)) + '\nQuantiles: ' + str(
+        quant(data))
 
 
-
-sg.theme("Purple")
+sg.theme("Default1")
 sg.SetOptions(element_padding=(3, 3))
 
 menu_def = [['File', ['Open', 'Save', 'Exit']],
-            ['Edit', ['Clear', ['Normal'], 'Undo'], ],
-            ['Help', 'About...'], ]
+            ['Show', ['Graph']],
+            ['Edit', ['Standartize', 'Shift', 'Clear', ['Normal'], 'Undo'], ],
+            ['Help', ['Log()', 'Anomalies', 'About...']]]
 
 layout = [
     [sg.Menu(menu_def)],
@@ -33,25 +34,112 @@ layout = [
 window = sg.Window('MathStat', layout, resizable=False, finalize=True, font="Courier 10", default_element_size=(6, 1),
                    default_button_element_size=(10, 1), size=(600, 400))
 arr = []
-
 while True:
     temp = []
     event, values = window.read()
+
+    if event == 'Shift':
+        try:
+            if arr[0] is not None:
+                step = sg.PopupGetText('Shift step:')
+                arr = arr + int(step)
+                window['-out-'].update(statistic_analysis(arr))
+        except:
+            sg.popup('Some error occured.')
+            pass
+
+    if event == 'Anomalies':
+        sg.Popup('Anomalies is auto-detected', title='Anomalies')
+
+    if event == 'Log()':
+        sg.popup('You can logarithm in figure settings.\n', title='Log()')
+
+    if event == 'Standartize':
+        try:
+            if arr[0] is not None:
+                st_arr = (arr - mean(arr)) / standart_d(arr)
+                window['-out-'].update(statistic_analysis(st_arr))
+        except:
+            sg.popup('Some error occured.\nMaybe already standartized?')
+            pass
+
     if event == 'Normal':
-        window['-out-'].update('')
+        try:
+            if arr[0] is not None:
+                window['-out-'].update('')
+                arr = []
+        except:
+            sg.popup('Some error occured.\nNO DATA!')
+            pass
+
     if event == 'About...':
         sg.Popup('This program was build by V.Shynkarov', title='About', )
+
     if event == 'Open':
         try:
             path = sg.popup_get_file('Open...')
-            arr = sorted(np.loadtxt(path))
+            arr = np.sort(np.loadtxt(path))
+            anom = find_anomalies(arr)
+            if anom:
+                for anomaly in anom:
+                    for item in arr:
+                        if float(anomaly) == float(item):
+                            arr = np.delete(arr, np.where(arr == item))
+                sg.popup('Anomalies where found and deleted!\n'
+                         f'{anom}')
             window['-out-'].update(statistic_analysis(arr))
         except:
             if event == 'Cancel':
                 pass
+
     if event == 'Save':
-        with open('sortedarray.txt', 'w') as f:
-            for item in arr:
-                f.write(str(item) + '\n')
+        try:
+            if arr[0] is not None:
+                with open('sortedarray.txt', 'w') as f:
+                    for item in arr:
+                        f.write(str(item) + '\n')
+        except:
+            sg.popup('Nothing to save')
+
+    if event == 'Graph':
+        try:
+            if arr[0] is not None:
+                plt.style.context('seaborn')
+                f = plt.figure(tight_layout=True)
+                f.set_figwidth(8)
+                f.set_figheight(8)
+                # fig1, ax = plt.subplots()
+                plt.subplot(2, 1, 1)
+                plt.grid(b=True, color='grey',
+                         linestyle='-.', linewidth=0.5,
+                         alpha=0.6)
+                plt.xlim([min(arr) - (min(arr) * 0.1), max(arr) + (min(arr) * 0.1)])
+                plt.xlabel('Value', fontsize=12)
+                plt.ylabel('Frequency', fontsize=12)
+                plt.tick_params(axis='both', which='major', labelsize=14)
+                '''
+                bins = sqrt(N) if N > 100
+                bins = N ** 1/3
+                '''
+                if len(arr) <= 100:
+                    plt.hist(arr, bins=(round(len(arr) ** (1 / 2))), weights=np.zeros_like(arr) + 1 / len(arr),
+                             edgecolor='#E6E6E6')
+                else:
+                    plt.hist(arr, bins=(round(len(arr) ** (1 / 3))), weights=np.zeros_like(arr) + 1 / len(arr),
+                             edgecolor='#E6E6E6')
+                x = np.linspace(0, max(arr))
+                plt.plot(x, norm.pdf(x, mean(arr), standart_d(arr)), color='red', linewidth=2)
+                # making emperical graph
+                plt.subplot(2, 1, 2)
+                plt.step(arr, np.arange(len(arr)) / float(len(arr)), linewidth=3)
+                plt.plot(arr, np.arange(len(arr)) / float(len(arr)), color='red', linewidth=2)
+                plt.ylim(0, 1)
+                plt.xlabel('Value', fontsize=12)
+                plt.ylabel('Frequency', fontsize=12)
+                plt.tick_params(axis='both', which='major', labelsize=14)
+                plt.show()
+        except:
+            sg.popup('Some error occured.\nNO DATA!')
+
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
--- stats.py
+++ stats.py
@@ -1,60 +1,84 @@
 import numpy as np
 import statistics
-def mid(mas):
-    return round(sum(mas) / len(mas), 4)
 
 
-def scale(mas):
-    return round(max(mas) - min(mas), 4)
+def mid(data):
+    return round(sum(data) / len(data), 4)
 
 
-def med(mas):
-    return round(statistics.median(mas), 4)
+def scale(data):
+    return round(max(data) - min(data), 4)
 
 
-def var(mas):
-    return round(statistics.variance(mas), 4)
+def med(data):
+    return round(statistics.median(data), 4)
 
 
-def me(mas):
-    return round(statistics.mean(mas), 4)
+def var(data):
+    return round(statistics.variance(data), 4)
 
 
-def standart_d(mas):
-    return round(statistics.stdev(mas), 4)
+def mean(data):
+    return round(statistics.mean(data), 4)
 
 
-def asymmetry(mas):
+def standart_d(data):
+    return round(statistics.stdev(data), 4)
+
+
+def asymmetry(data):
     buf = 0
     k = 0
-    while (k < len(mas)):
-        buf += (mas[k] - me(mas)) ** 3
+    while (k < len(data)):
+        buf += (data[k] - mean(data)) ** 3
         k += 1
-    asymmetry = (buf / len(mas)) / standart_d(mas) ** 3
+    asymmetry = (buf / len(data)) / standart_d(data) ** 3
     return round(asymmetry, 4)
 
 
-def kurtosis(mas):
+def kurtosis(data):
     buf = 0
     k = 0
-    while (k < len(mas)):
-        buf += (mas[k] - me(mas)) ** 4
+    while (k < len(data)):
+        buf += (data[k] - mean(data)) ** 4
         k += 1
-    kurtosis = (buf / len(mas)) / standart_d(mas) ** 4
+    kurtosis = (buf / len(data)) / standart_d(data) ** 4
     return round(kurtosis, 4)
 
 
-def c_kurtosis(mas):
-    return round(1 / abs(kurtosis(mas)) ** 0.5, 4)
+def c_kurtosis(data):
+    return round(1 / abs(kurtosis(data)) ** 0.5, 4)
 
 
-def pirson(mas):
-    return round(standart_d(mas) / me(mas), 4)
+def pirson(data):
+    if mean(data) != 0:
+        return round(standart_d(data) / mean(data), 4)
+    else:
+        return 'Mean=0'
 
 
-def med_oul(mas):
-    return round(mas[round(len(mas) / 2)], 4)
+def med_oul(data):
+    return round(data[round(len(data) / 2)], 4)
 
 
-def quant(mas):
-    return statistics.quantiles(mas)
+def quant(data):
+    return statistics.quantiles(data)
+
+
+def find_anomalies(data):
+    # define a list to accumlate anomalies
+    anomalies = []
+
+    # Set upper and lower limit to 3 standard deviation
+    random_data_std = standart_d(data)
+    random_data_mean = mean(data)
+    anomaly_cut_off = random_data_std * 3
+
+    lower_limit = random_data_mean - anomaly_cut_off
+    upper_limit = random_data_mean + anomaly_cut_off
+    # Generate outliers
+    for outlier in data:
+        if outlier > upper_limit or outlier < lower_limit:
+            anomalies.append(outlier)
+    return anomalies
+
