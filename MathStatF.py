50f01b67b9ea1f431d5b4b23310199632b3aa292
Volodymyr Shynkarov <djevl333@gmail.com>
2022-01-24 04:46:30 +0200

    Added image, added PDFs, new interface

--- MathStatF.py
+++ MathStatF.py
@@ -1,19 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.stats import norm
+import seaborn as sns
+# seaborn gives 'FutureWarning', importing warnings to get rid of it
+import warnings
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+from scipy import stats
 import PySimpleGUI as sg
 from stats import *
 
 
 def statistic_analysis(data):
     text = ''
-    text += 'Summ: ' + str(round(sum(data), 4)) + '\nMax: ' + str(round(max(data), 5)) + '\nMin: ' + str(
-        round(min(data), 5)) + '\nMid: ' + str(mid(data)) + '\nScale: ' + str(
+    text += 'Length: ' + str(len(data)) + '\nSumm: ' + str(round(sum(data), 2)) + '\nMax: ' + str(
+        round(max(data), 5)) + '\nMin: ' + str(
+        round(min(data), 5)) + '\nScale: ' + str(
         scale(data)) + '\nMean: ' + str(mean(data)) + '\nMed: ' + str(med(data)) + '\nVar: ' + str(
         var(data)) + '\nStDev: ' + str(standart_d(data)) + '\nAsym: '
     return text + str(asymmetry(data)) + '\nEcs: ' + str(
         kurtosis(data)) + '\nCEcs: ' + str(
-        c_kurtosis(data)) + '\nPirson: ' + str(pirson(data)) + '\nMedU: ' + str(med_oul(data)) + '\nQuantiles: ' + str(
+        c_kurtosis(data)) + '\nVarPirs: ' + str(pirson(data)) + '\nMedU: ' + str(med_oul(data)) + '\nQuantiles: ' + str(
         quant(data))
 
 
@@ -23,20 +29,48 @@ sg.SetOptions(element_padding=(3, 3))
 menu_def = [['File', ['Open', 'Save', 'Exit']],
             ['Show', ['Graph']],
             ['Edit', ['Standartize', 'Shift', 'Clear', ['Normal'], 'Undo'], ],
-            ['Help', ['Log()', 'Anomalies', 'About...']]]
+            ['Tests', ['Pearson Chi-squared', 'Kolmogorov-Smirnov', 'F-test for 2 variances', 'Bartletts',
+                       'Wilcoxon signed-rank', 'Sign', 'Single factor analysis of variance', 'Kruskal-Wallis (H)',
+                       'T-test', ['Hypothesis', '2-sample']]],
+            ['Help', ['Log()', 'Tests',
+                      ['Pearson', 'Kolmogorov', 'F-test', 'Bartletts', 'Wilcoxon', 'Sign', 'Single factor', 'H-test',
+                       'T-test', ['Hyp.', '2-sample']], 'Anomalies', 'About...']]]
 
 layout = [
     [sg.Menu(menu_def)],
     [sg.Multiline(tooltip='Statistical analysis', size=(15, 40), key='-out-', disabled=True, no_scrollbar=True,
-                  font='Courier 12')],
+                  font='Courier 12'), sg.Image('mathstat.png', pad=(105,15), key='-Image-')],
 ]
 
-window = sg.Window('MathStat', layout, resizable=False, finalize=True, font="Courier 10", default_element_size=(6, 1),
+window = sg.Window('Mathematical Statistics', layout, resizable=False, finalize=True, font="Courier 10",
+                   icon='math.ico',
+                   default_element_size=(6, 1),
                    default_button_element_size=(10, 1), size=(600, 400))
 arr = []
+temp = []
+dimension = 0
 while True:
-    temp = []
     event, values = window.read()
+    help_info(event)
+
+    if event == 'Pearson Chi-squared':
+        try:
+            if dimension == 3:
+                pass
+            elif dimension == 2:
+                pass
+            else:
+                sg.popup('You need atleast 2-dimensional array', icon='math.ico')
+        except:
+            sg.Popup('NO DATA!', icon='math.ico')
+
+    if event == 'Undo':
+        try:
+            last_event = event
+            arr = temp
+            window['-out-'].update(statistic_analysis(arr))
+        except:
+            sg.popup_quick('Nothing to undo!', icon='math.ico')
 
     if event == 'Shift':
         try:
@@ -45,22 +79,17 @@ while True:
                 arr = arr + int(step)
                 window['-out-'].update(statistic_analysis(arr))
         except:
-            sg.popup('Some error occured.')
+            sg.popup_quick('Some error occured.\nNO DATA!', icon='math.ico')
             pass
-
-    if event == 'Anomalies':
-        sg.Popup('Anomalies is auto-detected', title='Anomalies')
-
-    if event == 'Log()':
-        sg.popup('You can logarithm in figure settings.\n', title='Log()')
 
     if event == 'Standartize':
         try:
             if arr[0] is not None:
-                st_arr = (arr - mean(arr)) / standart_d(arr)
-                window['-out-'].update(statistic_analysis(st_arr))
+                last_event = event
+                arr = (arr - mean(arr)) / standart_d(arr)
+                window['-out-'].update(statistic_analysis(arr))
         except:
-            sg.popup('Some error occured.\nMaybe already standartized?')
+            sg.popup('Some error occured.', icon='math.ico')
             pass
 
     if event == 'Normal':
@@ -69,24 +98,36 @@ while True:
                 window['-out-'].update('')
                 arr = []
         except:
-            sg.popup('Some error occured.\nNO DATA!')
+            sg.popup_quick('Some error occured.\nNO DATA!')
             pass
-
-    if event == 'About...':
-        sg.Popup('This program was build by V.Shynkarov', title='About', )
 
     if event == 'Open':
         try:
-            path = sg.popup_get_file('Open...')
-            arr = np.sort(np.loadtxt(path))
-            anom = find_anomalies(arr)
-            if anom:
-                for anomaly in anom:
-                    for item in arr:
-                        if float(anomaly) == float(item):
-                            arr = np.delete(arr, np.where(arr == item))
-                sg.popup('Anomalies where found and deleted!\n'
-                         f'{anom}')
+            path = sg.popup_get_file('Open...', icon='math.ico')
+            dimension = check_for_columns(path)
+            if dimension == 3:
+                arr, arr1, arr2 = np.sort(np.loadtxt(path, unpack=True))
+                sg.popup_ok('3-dimensional', icon='math.ico')
+            elif dimension == 2:
+                arr, arr1 = np.sort(np.loadtxt(path, unpack=True))
+                sg.popup_ok('2-dimensional', icon='math.ico')
+            else:
+                arr = np.sort(np.loadtxt(path))
+                sg.popup_ok('1-dimensional', icon='math.ico')
+
+            # anom = find_anomalies(arr)
+            # find_anomalies(arr1)
+
+            # check for anomalies by 3-sigma rule
+
+            # if anom:
+            #     for anomaly in anom:
+            #         for item in arr:
+            #             if float(anomaly) == float(item):
+            #                 arr = np.delete(arr, np.where(arr == item))
+            #     sg.popup('Anomalies where found and deleted!\n'
+            #              f'{anom}')
+            temp = arr
             window['-out-'].update(statistic_analysis(arr))
         except:
             if event == 'Cancel':
@@ -98,17 +139,42 @@ while True:
                 with open('sortedarray.txt', 'w') as f:
                     for item in arr:
                         f.write(str(item) + '\n')
+                sg.popup('File saved as:\n'
+                         'sortedarray.txt', icon='math.ico')
         except:
-            sg.popup('Nothing to save')
+            sg.popup_quick('Nothing to save', icon='math.ico')
 
     if event == 'Graph':
+        """
+        
+        x = np.linspace(min(arr), max(arr), len(arr))
+        plt.plot(x, norm.pdf(x, mean(arr), standart_d(arr)), color='red', linewidth=2)
+        plt.hist(arr, bins=(int(len(arr) ** (1 / 2))), weights=np.zeros_like(arr) + 1 / len(arr),
+                edgecolor='#E6E6E6')
+        pdf = normpdf(x,mean(arr), standart_d(arr))
+        plt.step(arr, np.arange(len(arr)) / float(len(arr)), linewidth=3)
+        plt.plot(arr, np.arange(len(arr)) / float(len(arr)), color='red', linewidth=1)
+        
+        plt.plot(x, norm.pdf(x, mean(arr), standart_d(arr)), color='red', linewidth=2)
+                    plt.hist(arr, bins=(int(len(arr) ** (1 / 2))), weights=np.zeros_like(arr) + 1 / len(arr),
+                             edgecolor='#E6E6E6')
+        
+        """
         try:
             if arr[0] is not None:
+                try:
+                    if last_event == 'Standartize':
+                        sg.popup_ok('Standartized data', icon='math.ico')
+                except:
+                    pass
+
                 plt.style.context('seaborn')
                 f = plt.figure(tight_layout=True)
                 f.set_figwidth(8)
                 f.set_figheight(8)
                 # fig1, ax = plt.subplots()
+                # sns.set_theme(style="ticks")
+                sns.set_theme(style="whitegrid")
                 plt.subplot(2, 1, 1)
                 plt.grid(b=True, color='grey',
                          linestyle='-.', linewidth=0.5,
@@ -118,28 +184,32 @@ while True:
                 plt.ylabel('Frequency', fontsize=12)
                 plt.tick_params(axis='both', which='major', labelsize=14)
                 '''
+                
                 bins = sqrt(N) if N > 100
                 bins = N ** 1/3
+                
                 '''
+                x = np.linspace(min(arr), max(arr), len(arr))
                 if len(arr) <= 100:
-                    plt.hist(arr, bins=(round(len(arr) ** (1 / 2))), weights=np.zeros_like(arr) + 1 / len(arr),
-                             edgecolor='#E6E6E6')
+                    sns.distplot(arr, bins=(int(len(arr) ** (1 / 2))), kde=True,
+                                 hist_kws={'alpha': 0.6, 'color': 'g'})
+                    x = np.linspace(min(arr), max(arr), int(len(arr) ** (1 / 2)))
                 else:
-                    plt.hist(arr, bins=(round(len(arr) ** (1 / 3))), weights=np.zeros_like(arr) + 1 / len(arr),
-                             edgecolor='#E6E6E6')
-                x = np.linspace(0, max(arr))
-                plt.plot(x, norm.pdf(x, mean(arr), standart_d(arr)), color='red', linewidth=2)
+                    sns.distplot(arr, bins=(int(len(arr) ** (1 / 3)) - 1), kde=True,
+                                 hist_kws={'alpha': 0.6, 'color': 'g'})
+                    x = np.linspace(min(arr), max(arr), int(len(arr) ** (1 / 3)) - 1)
+
                 # making emperical graph
+
                 plt.subplot(2, 1, 2)
-                plt.step(arr, np.arange(len(arr)) / float(len(arr)), linewidth=3)
-                plt.plot(arr, np.arange(len(arr)) / float(len(arr)), color='red', linewidth=2)
-                plt.ylim(0, 1)
+                sns.ecdfplot(x)
+                sns.ecdfplot(arr)
                 plt.xlabel('Value', fontsize=12)
                 plt.ylabel('Frequency', fontsize=12)
                 plt.tick_params(axis='both', which='major', labelsize=14)
                 plt.show()
         except:
-            sg.popup('Some error occured.\nNO DATA!')
+            sg.popup_quick('Some error occured.\nNO DATA!', icon='math.ico')
 
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
--- stats.py
+++ stats.py
@@ -1,9 +1,6 @@
 import numpy as np
+import PySimpleGUI as sg
 import statistics
-
-
-def mid(data):
-    return round(sum(data) / len(data), 4)
 
 
 def scale(data):
@@ -62,23 +59,85 @@ def med_oul(data):
 
 
 def quant(data):
-    return statistics.quantiles(data)
+    try:
+        temp = [round(x, 2) for x in statistics.quantiles(data)]
+        return temp
+    except:
+        pass
 
 
 def find_anomalies(data):
-    # define a list to accumlate anomalies
     anomalies = []
-
-    # Set upper and lower limit to 3 standard deviation
-    random_data_std = standart_d(data)
-    random_data_mean = mean(data)
-    anomaly_cut_off = random_data_std * 3
-
-    lower_limit = random_data_mean - anomaly_cut_off
-    upper_limit = random_data_mean + anomaly_cut_off
-    # Generate outliers
+    anomaly_cut_off = standart_d(data) * 3
+    lower_limit = mean(data) - anomaly_cut_off
+    upper_limit = mean(data) + anomaly_cut_off
     for outlier in data:
         if outlier > upper_limit or outlier < lower_limit:
             anomalies.append(outlier)
     return anomalies
 
+
+import math
+
+
+def normpdf(x, mean, sd):
+    var = float(sd) ** 2
+    denom = (2 * math.pi * var) ** .5
+    num = math.exp(-(float(x) - float(mean)) ** 2 / (2 * var))
+    return num / denom
+
+
+def check_for_columns(txt):
+    try:
+        test2 = []
+        test1 = []
+        try:
+            test, test1, test2 = np.loadtxt(txt, unpack=True)
+        except ValueError:
+            pass
+        try:
+            test, test1 = np.loadtxt(txt, unpack=True)
+        except ValueError:
+            pass
+
+        if type(test2) != list:
+            return 3
+        elif type(test1) != list:
+            return 2
+        else:
+            return 1
+    except:
+        return 0
+
+
+def help_info(event):
+    if event == 'Anomalies':
+        sg.Popup('Anomalies is auto-detected', title='Anomalies', icon='math.ico')
+
+    if event == 'Log()':
+        sg.popup('You can logarithm in figure settings.\n', title='Log()', icon='math.ico')
+
+    if event == 'About...':
+        sg.popup('This program was build by V.Shynkarov', title='About', icon='math.ico')
+
+    if event == 'Pearson':
+        sg.popup_no_buttons(
+            "The chi-square test for independence, also called Pearson's chi-square test or the chi-square test of association, is used to discover if there is a relationship between two categorical variables.",
+            title='Pearson', icon='math.ico', image='Pearson.png')
+
+    if event == 'Kolmogorov':
+        sg.popup_no_buttons(
+            "Kolmogorov–Smirnov test (K–S test or KS test): is used to decide if a sample comes from a population with a specific distribution.one-dimensional probability distributions that can be used to compare a sample with a reference probability distribution (one-sample K–S test), or to compare two samples (two-sample K–S test). In essence, the test answers the question 'What is the probability that this collection of samples could have been drawn from that probability distribution?' or, in the second case, 'What is the probability that these two sets of samples were drawn from the same (but unknown) probability distribution?'",
+            title='Kolmogorov', icon='math.ico')
+        # if event == 'Pearson':
+        # if event == 'Pearson':
+        # if event == 'Pearson':
+        # if event == 'Pearson':
+        # if event == 'Pearson':
+        # if event == 'Pearson':
+        # if event == 'Pearson':
+        # if event == 'Pearson':
+        #     if event == '':
+        #         sg.popup_no_buttons(
+        #             "",
+        #             title='', icon='math.ico')
